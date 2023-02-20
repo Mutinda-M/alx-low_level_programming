27cd8066965e4cc11900208fb9c753a8041fa8e8
Mutuku Kennedy <mutukukennedy053@gmail.com>
2023-02-20 11:32:11 -0800

    puts_command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
-#include<stdio.h>
+include<stdio.h>
 
+/**
+ *main - Entry
+ *
+ * puts - put string
+ *
+ * Return (o) - always success
+ */
 int main(void)
-	{
-		puts("\"Programming is like building a multilingual puzzle");
-		return(0);
-	}
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
