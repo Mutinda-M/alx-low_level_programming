a60070598288e0f8a300122e95909347d91ac265
Mutuku Kennedy <mutukukennedy053@gmail.com>
2023-02-21 12:26:31 -0800

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print all small letter excluding q and e
+ *
+ * Return: Return 0 when correct
+ */
+int main(void)
+{
+	char d = 'a';
+
+	while (d <= 'z';)
+	{
+		if (d != e && d != q)
+		{
+			putchar(d);
+			d++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
