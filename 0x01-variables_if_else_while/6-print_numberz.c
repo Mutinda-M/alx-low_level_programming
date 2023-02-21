e61ccc18bb2424720dcc60fa2c1f9187505348e8
Mutuku Kennedy <mutukukennedy053@gmail.com>
2023-02-21 14:01:26 -0800

    numbz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: return 0 when correct
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
