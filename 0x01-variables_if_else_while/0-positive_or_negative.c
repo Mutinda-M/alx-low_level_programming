3c587b78acf3c177a2159359b99a36045a8395df
Mutuku Kennedy <mutukukennedy053@gmail.com>
2023-02-20 17:29:24 -0800

    if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,6 @@
 #include<stdio.h>
 
 /**
- *
  * main - Determine if a random number is positive, negative or zero
  *
  *
@@ -15,11 +14,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-		if(n > 0)
+		if (n > 0)
 		{
 			printf("%d is positive\n", n);
 		}
-		else if(n == 0)
+		else if (n == 0)
 		{
 			printf("%d is zero\n", n);
 		}
