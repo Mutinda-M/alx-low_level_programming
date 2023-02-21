8abddd648f9c7abd286957e0ada0903c76b3b048
Mutuku Kennedy <mutukukennedy053@gmail.com>
2023-02-20 18:54:38 -0800

    alphA

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints in both lower case and upper case
+ *
+ * Return: Returns 0 when correct
+ */
+int main(void)
+{
+	for (x = 'a'; x <= 'z'; x++);
+	{
+		putchar(x);
+	}
+	for (y  = 'A'; y <= 'Z'; y++);
+	{
+		putcher(y);
+	}
+	putcher('\n');
+	return (0);
+}	
