340110d6fb08638d541d2e7c8e879a9ff9502e26
Mutuku Kennedy <mutukukennedy053@gmail.com>
2023-02-22 15:56:29 -0800

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * main - 
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char C;
+	
+	for (C = 'a'; C <= 'z'; C++);
+	{
+		putchar(C);
+	}
+	putchar('\n');
+	return(0);
+}
