c2a6ff35737a183728d44df3f18d31c43f12a8eb
Mutuku Kennedy <mutukukennedy053@gmail.com>
2023-02-23 07:23:23 -0800

    HOT

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <studio.h>
 /**
  * main - Entry point
  *
@@ -7,6 +6,13 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
 	return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,7 +10,7 @@ void print_alphabet(void)
 	
 	for (C = 'a'; C <= 'z'; C++)
 	{
-		putchar(C);
+		_putchar(C);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
