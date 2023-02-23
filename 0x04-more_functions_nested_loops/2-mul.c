d6e71cde93594e69e86674b520fa237cd7eb2e7e
Mutuku Kennedy <mutukukennedy053@gmail.com>
2023-02-23 11:44:53 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *mul - multplies specified integers
+ *@a - multiplies by b
+ *@b - multiplies by a
+ * Return:
+ */
+int mul(int a, int b)
+{
+	int c;
+	c = a * b;
+	return (c);
+}
