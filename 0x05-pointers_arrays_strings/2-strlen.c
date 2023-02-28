853484c8ace9aa9bf33c3b0defe1080090972d33
Mutuku Kennedy <mutukukennedy053@gmail.com>
2023-02-28 10:07:45 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - calculates langth of string with pointer *s
+ * @s: pointer to string
+ * Return: returns 0nif successful
+ */
+int _strlen(char *s)
+{
+	int _strlen(*s);
+
+	return(0);
+}
