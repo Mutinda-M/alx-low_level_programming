b603bd6ee7753e5878a06adad9e620d215ca2f70
Mutuku Kennedy <mutukukennedy053@gmail.com>
2023-03-01 01:15:56 -0800

    str

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * _puts  - prints string
- * @i: integer for loop
+ * @str: string
  * Return: void
  */
 void _puts(char *str)
 {
 	int i = 0;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
