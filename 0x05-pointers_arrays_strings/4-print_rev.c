db9bcc0248e5dfec448377c79c77511465332898
Mutuku Kennedy <mutukukennedy053@gmail.com>
2023-03-01 02:13:43 -0800

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,7 +6,7 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int i, n;
 
 	i = 0;
 
@@ -14,7 +14,9 @@ void print_rev(char *s)
 	{
 		i++;
 	}
-		i--;
-		_putchar(s[i]);
+	for (n = i -1; n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
 	_putchar('\n');
 }
