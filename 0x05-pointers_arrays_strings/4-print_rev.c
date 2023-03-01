692d859de14982ffb094a29a9851ed1fd415e5bb
Mutuku Kennedy <mutukukennedy053@gmail.com>
2023-03-01 01:43:56 -0800

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_rev - prints string in reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
