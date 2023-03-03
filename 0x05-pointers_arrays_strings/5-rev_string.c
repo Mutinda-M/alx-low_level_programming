daa0f057af6dcf1147e6fdcecea5d1b9bda0ad91
Mutuku Kennedy <mutukukennedy053@gmail.com>
2023-03-03 03:00:30 -0800

    revstr

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_string- reverses a string
+ * @s: string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int n, m;
+	char s;
+
+	n = 0;
+	while (s[n]  =! '\0')
+	{
+		n++;
+	}
+	for (m = n - 1; m >= 0; m--)
+	{
+		s[m];
+	}
+}
