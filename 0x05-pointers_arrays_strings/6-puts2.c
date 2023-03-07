077692eb2bf8da065da2f5bb814a962d549cda0a
Mutuku Kennedy <mutukukennedy053@gmail.com>
2023-03-07 10:46:05 -0800

    rev

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,15 +6,15 @@
  * Return: void
  */
 void puts2(char *str)
-{	
+{
 	int M;
 
-	for(M = 0; str[M]!= '\0'; M++)
-	{
+	for(M = 0; str[M] != '\0'; M++)
+	
 	if(M % 2 == 0)
-	{
+
 		_putchar(str[M]);
-	}
+
 	_putchar('\0');
-	}
+
 }
